79b07e650930df72e58459b9784c083746c706d1
Moisesfeitosa <61851544+Moisesfeitosa@users.noreply.github.com>
2025-12-05 19:57:30 -0300

    Update função_detecção_casos_problematicos_SK.R

--- função_detecção_casos_problematicos_SK.R
+++ função_detecção_casos_problematicos_SK.R
@@ -1,57 +1,3 @@
-library(readxl)
-library(tidyverse)
-library(multcompView)
-library(emmeans)
-library(DHARMa)
-library(multcomp)
-library(gvlma)
-library(DescTools)
-library(ScottKnott)
-
-# importando os dados
-teste<- read_xlsx("C:/Users/moise/OneDrive/Documentos/teste.xlsx")
-
-teste$media<- as.numeric(teste$media)
-teste$trt<- as.factor(teste$trt)
-
-
-modelo1<- lm(media ~ trt, data= teste)
-summary(modelo1)
-modelo2<-lm(media ~ trt, data= teste2)
-anova(modelo1)
-plot(modelo1)
-ggqqplot(modelo1$residuals)
-gvlma(modelo1)
-simulateResiduals(modelo1, plot= TRUE)
-
-# conduzindo o tukey
-emmeans(modelo1, specs = ~trt) %>%
-  cld(method= "tukey", Letters= letters, alpha= 0.05, decreasing= TRUE)
-
-
-# contraste usando DescTools
-todos<- DunnettTest(x= teste$media, g= teste$trt)
-
-
-# agora excluindo o controle
-excl<- teste %>%
-  filter(trt != "Controle")
-
-
-emmeans(modelo1, specs= ~trt) %>% contrast(method= "tukey")
-?pairs
-contraste_exc<- emmeans(modelo2, specs= ~trt) %>% pairs()
-contraste_todos
-contraste_exc
-
-
-
-# conduzindo um Scott-Knott
-summary(SK(x= modelo1, which= "trt"))
-detect_p(tabela_contraste)
-
-
-
 detect_p2 <- function(table){
   #table$trt<- as.factor(table$trt)
   modelo1<- lm(media ~ trt, data= table)
@@ -80,3 +26,4 @@ detect_p2 <- function(table){
   }
 
 }
+
